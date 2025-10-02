e1b5e19dcc5efbb3ab064ee80d948b5af20cbcf9
singilwon <joowoni98@naver.com>
2025-10-02 17:20:53 +0900

    🎉 Initial Commit

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      "no-undef": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrors: "none",
+        },
+      ],
+      eqeqeq: ["error", "always"],
+      "no-var": "error",
+      "prefer-const": ["error", { destructuring: "all" }],
+      "no-constant-condition": ["error", { checkLoops: false }],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": "warn",
+      curly: ["error", "multi-line"],
+      "object-shorthand": ["warn", "always"],
+      "dot-notation": "warn",
+      "prefer-template": "warn",
+      "import/no-unresolved": "off",
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/no-floating-promises": "error",
+    },
+  },
+]);
