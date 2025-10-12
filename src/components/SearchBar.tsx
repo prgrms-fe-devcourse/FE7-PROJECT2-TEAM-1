faf252f88b4618921f4385a8ec97a793ebaabb2c
singilwon <joowoni98@naver.com>
2025-10-12 23:53:22 +0900

    ✅ feat: search page UI

--- src/components/SearchBar.tsx
+++
@@ -1,7 +0,0 @@
-export default function SearchBar() {
-  return (
-    <>
-      <h1>SearchBar Component</h1>
-    </>
-  );
-}
--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,5 +1,15 @@
 import { Outlet } from "react-router";
+import Header from "../components/Header";
 
 export default function Default() {
-  return <Outlet />;
+  return (
+    <div className="bg-black text-white">
+      <div className="border-b-2 border-[#FF8C00]">
+        <Header />
+      </div>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
