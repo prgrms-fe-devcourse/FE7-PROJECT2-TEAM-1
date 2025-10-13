39e0600fc4f126e98be896ad4385d3fde0bd4b4c
singilwon <joowoni98@naver.com>
2025-10-14 00:29:33 +0900

    ✅ feat: modal sure UI

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,15 +1,7 @@
-import logo from "../assets/header/header_logo.svg";
-
 export default function NotFound() {
   return (
     <>
-      <div className="bg-[#000000] flex items-center justify-center h-screen">
-        <div className=" bg-[#0A0A0A] w-[1200px] h-[619px] border-2 border-[rgb(255_140_0_/_0.5)] rounded-[12px]  flex flex-col items-center justify-center">
-          <img src={logo} className="w-[150px] h-[225px]" />
-          <p className="text-[36px] text-[#FF8C00]">404 ERROR</p>
-          <p className=" text-[16px] text-[#999999] mt-6">잘못된 접근입니다!</p>
-        </div>
-      </div>
+      <h1>NotFound Component</h1>
     </>
   );
 }
