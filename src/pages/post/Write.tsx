2f27335f379d33d0e2d5a0366a3d59c799a3b2ec
varYeon <xjkooy100@gmail.com>
2025-10-14 16:49:10 +0900

    🚧 블럭 간격 조정 및 아이콘 추가, div 태그 수정

--- src/pages/post/Write.tsx
+++ src/pages/post/Write.tsx
@@ -1,3 +1,5 @@
+import uploadButton from "../../assets/write/upload_button.svg";
+
 export default function Write() {
   return (
     <>
@@ -6,64 +8,71 @@ export default function Write() {
           <span>(화살표)새 밸런스 게임 만들기</span>
         </div>
 
-        <div className="w-300 h-248 border-2 border-[#FF8C00]">
+        <div className="w-[1250px] h-[1150px] px-[59px] py-[35px] border-2 border-[#FF8C00]">
           <div>
+            <p className="mb-[10px]">주제 선택</p>
+            <select className="w-[202px] h-[36px] mb-[30px] border-2 border-[#FF8C00]">
+              <option>주제를 선택하세요</option> {/* 선택 안 되도록 */}
+              <option>우정</option>
+              <option>연애</option>
+              <option>음식</option>
+              <option>생활</option>
+              <option>취미</option>
+              <option>일</option>
+            </select>
+            <p className="mb-[10px]">제목</p>
+            <input
+              placeholder="예: 치킨 vs 피자, 당신의 선택은?"
+              className="w-[1138px] h-[36px] mb-[10px] border-2 border-[#FF8C00]"
+            ></input>
+            <p className="mb-[30px]">0/20</p>
+            <p className="mb-[10px]">설명</p>
+            <input
+              placeholder="밸런스 게임에 대한 설명을 입력하세요"
+              className="w-[1138px] h-[96px] mb-[10px] border-2 border-[#FF8C00]"
+            ></input>
+            <p className="mb-[46px]">0/200</p>
+          </div>
+
+          <div className="flex flex-cols-3 items-center mb-[42px]">
+            <div className="w-[540px] h-[1px] bg-[#FF8C00]"></div>
+            <p className="text-[#FF8C00]">선택지</p>
+            <div className="w-[540px] h-[1px] bg-[#FF8C00]"></div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-[82px] justify-items-center">
             <div>
-              <p>주제 선택</p>
-              <select className="w-50.5 h-9 border-2 border-[#FF8C00]">
-                <option>주제를 선택하세요</option> {/* 선택 안 되도록 */}
-                <option>우정</option>
-                <option>연애</option>
-                <option>음식</option>
-                <option>생활</option>
-                <option>취미</option>
-                <option>일</option>
-              </select>
+              <div className="mb-[16px]">
+                <p>(아이콘)선택지 A</p>
+              </div>
+              <input
+                placeholder="선택지 텍스트"
+                className="w-[528px] h-[36px] mb-[10px] border-2 border-[#FF8C00]"
+              ></input>
+              <p className="mb-[16px]">0/15</p>
+              <img src={uploadButton} className=" w-[35px] h-[35px]" />
+              <p className="w-[528px] h-[322px] mb-[60px] border-2 border-dashed border-[#FF8C00]">
+                이미지업로드
+              </p>
             </div>
             <div>
-              <p>제목</p>
+              <div className="mb-[16px]">
+                <p>(아이콘)선택지 B</p>
+              </div>
               <input
-                placeholder="예: 치킨 vs 피자, 당신의 선택은?"
-                className="w-284.5 h-9 border-2 border-[#FF8C00]"
+                placeholder="선택지 텍스트"
+                className="w-[528px] h-[36px] mb-[10px] border-2 border-[#FF8C00]"
               ></input>
-            </div>
-            <div>
-              <p>설명</p>
-              <input
-                placeholder="밸런스 게임에 대한 설명을 입력하세요"
-                className="w-284.5 h-24 border-2 border-[#FF8C00]"
-              ></input>
+              <p className="mb-[16px]">0/15</p>
+              <img src={uploadButton} className=" w-[35px] h-[35px]" />
+              <p className="w-[528px] h-[322px]  mb-[60px] border-2 border-dashed border-[#FF8C00]">
+                이미지업로드
+              </p>
             </div>
           </div>
 
           <div className="grid justify-items-center">
-            <div className="flex-1 border-t border-gray-400"></div>
-            <p>선택지(양쪽으로 구분선)</p>
-            <div className="flex-1 border-t border-gray-400"></div>
-          </div>
-
-          <div className="grid grid-cols-2 gap-5.13 justify-items-center">
-            <div>
-              <p>(아이콘)선택지 A</p>
-              <input
-                placeholder="선택지 텍스트"
-                className="w-132 h-9 border-2 border-[#FF8C00]"
-              ></input>
-              <p className="w-132 h-80.5 border-2 border-dashed border-[#FF8C00]">(이미지업로드)</p>
-            </div>
-            <div>
-              <p>(아이콘)선택지 B</p>
-              <input
-                placeholder="선택지 텍스트"
-                className="w-132 h-9 border-2 border-[#FF8C00]"
-              ></input>
-              <p className="w-132 h-80.5 border-2 border-dashed border-[#FF8C00]">(이미지업로드)</p>
-            </div>
-          </div>
-
-          <div className="grid justify-items-center">
-            <button className="w-106.5 h-10.25" />
-            게시하기
+            <button className="w-[426px] h-[41px] bg-[#FF8C00]">게시하기 </button>
           </div>
         </div>
       </div>
