285aec29ec656a402a943cdb3631ecd12bdbec1f
varYeon <xjkooy100@gmail.com>
2025-10-20 11:12:16 +0900

    ✅ 작성 중인 내용이 존재할 때 뒤로가면 모달 생성

--- src/pages/post/Write.tsx
+++ src/pages/post/Write.tsx
@@ -6,6 +6,7 @@ import supabase from "../../utils/supabase";
 import { useAuthStore } from "../../stores/authStore";
 import { useLocation, useNavigate } from "react-router";
 import Toast from "../../components/toast/Toast";
+import Sure from "../../components/modal/Sure";
 
 type Choices = { key: string; label: string; image: string }[];
 
@@ -43,6 +44,7 @@ export default function Write() {
   const [writeSelectTextB, setWriteSelectTextB] = useState("");
 
   const [showError, setShowError] = useState(false);
+  const [showSureModal, setShowSureModal] = useState(false);
 
   const profile = useAuthStore((state) => state.profile);
 
@@ -66,12 +68,23 @@ export default function Write() {
     }
   };
 
+  const writeEmptyHandler = () => {
+    const isError =
+      writeOption === "" ||
+      writeTitle === "" ||
+      writeExplain === "" ||
+      writeSelectTextA === "" ||
+      writeSelectTextB === "";
+
+    if (isError) return setShowError(true);
+  };
+
   const writeDataHandler = async () => {
-    if (writeOption === "") return setShowError(true);
-    if (writeTitle === "") return setShowError(true);
-    if (writeExplain === "") return setShowError(true);
-    if (writeSelectTextA === "") return setShowError(true);
-    if (writeSelectTextB === "") return setShowError(true);
+    if (writeOption === "") return;
+    if (writeTitle === "") return;
+    if (writeExplain === "") return;
+    if (writeSelectTextA === "") return;
+    if (writeSelectTextB === "") return;
     if (!profile) return;
     try {
       const { data, error } = await supabase
@@ -177,13 +190,36 @@ export default function Write() {
 
   return (
     <>
+      {showSureModal && (
+        <Sure
+          onClose={() => {
+            setShowSureModal(false);
+          }}
+          // TODO: yes 처리 안 함
+        />
+      )}
+
       <div className="flex flex-col items-center">
         <div className="w-full max-w-[1200px] my-9 mx-auto flex gap-5 items-center">
           <img
             src={categoryArrow}
             className="w-[31px] h-[26px] mt-[7px] cursor-pointer"
-            onClick={() => {
-              navigate(-1);
+            onClick={(isError) => {
+              console.log("writeOption:", writeOption);
+              console.log("writeTitle:", writeTitle);
+              console.log("writeExplain:", writeExplain);
+              console.log("writeSelectTextA:", writeSelectTextA);
+              console.log("writeSelectTextB:", writeSelectTextB);
+
+              writeEmptyHandler();
+              console.log("isError 상태:", !!isError);
+              isError ? setShowSureModal(true) : navigate(-1);
+              // TODO
+              // 다 비어 있을 때 모달로 넘어가는건 따로 조건 처리 필요
+              // TODO
+              // 디버깅 : 전부 차 있을 때는 false여야 하는데 true가 됨
+              // TODO
+              // 모달이 드롭다운 한테 묻힘 (위치 조정 필요)
             }}
           />
           <p className="w-[1000px] text-[#FF8C00] text-3xl">새 밸런스 게임 만들기</p>
@@ -360,7 +396,10 @@ export default function Write() {
             <button
               className="w-[426px] h-[41px] mt-[35px] bg-[#FF8C00] text-black rounded-md
               cursor-pointer transition-shadow duration-200 hover:scale-101 hover:drop-shadow-[0_0_5px_#ff8c00]"
-              onClick={() => writeDataHandler()}
+              onClick={() => {
+                writeDataHandler();
+                writeEmptyHandler();
+              }}
             >
               게시하기
             </button>
