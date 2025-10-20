208aef890193e90a166829477e07411a26c09ffd
singilwon <joowoni98@naver.com>
2025-10-20 11:17:34 +0900

    ✅ feat: postAlarm 올라가는 애니메이션 구현

--- src/components/modal/PostAlarm.tsx
+++ src/components/modal/PostAlarm.tsx
@@ -1,5 +1,6 @@
 import { createPortal } from "react-dom";
 import PostCard from "../../pages/post/PostCard";
+import { useCallback, useEffect, useState } from "react";
 
 export default function PostAlarm({
   setOpenPost,
@@ -8,13 +9,36 @@ export default function PostAlarm({
   setOpenPost: React.Dispatch<React.SetStateAction<boolean>>;
   post: Post | null;
 }) {
+  const [opening, setOpening] = useState(true);
+  const [closing, setClosing] = useState(false);
+  useEffect(() => {
+    const t = setTimeout(() => {
+      setOpening(false);
+    }, 10);
+    return () => clearTimeout(t);
+  }, []);
+
+  const handleCloseAnimation = useCallback(
+    (e: React.MouseEvent) => {
+      e?.stopPropagation;
+      if (closing) return;
+      setClosing(true);
+      setTimeout(() => {
+        setOpenPost(false);
+      }, 350);
+    },
+    [setOpenPost, closing],
+  );
+
   const modal = (
     <div className="fixed inset-0 z-[1000] bg-[rgb(132_124_124_/_0.3)] flex justify-center items-center font-normal">
-      <div className="w-[1200px] bg-black border-2 border-[#FF8C00] flex justify-center items-center font-normal p-10 flex-col gap-10">
+      <div
+        className={`w-[1200px] bg-black border-2 border-[#FF8C00] flex justify-center items-center rounded-[12px] shadow-[20px_20px_4px_rgba(0,0,0,0.25)] transition-all duration-300 ease-out font-normal p-10 flex-col gap-10 ${closing ? "opacity-0 translate-y-10" : opening ? "opacity-0 translate-y-10" : "opacity-100 translate-y-0"}`}
+      >
         {post && <PostCard post={post} />}
         <button
           className="hover:bg-[#FF8C00] hover:text-black cursor-pointer rounded-[5px] bg-[#AAAAAA] w-[150px] h-[50px] text-[25px] transition-all duration-250"
-          onClick={() => setOpenPost(false)}
+          onClick={handleCloseAnimation}
         >
           확인
         </button>
