5875e1b768a786143c203c6d0c2f164207bd60c9
varYeon <xjkooy100@gmail.com>
2025-10-21 21:31:04 +0900

    💄 style: header 로고 gap 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -58,17 +58,17 @@ export default function Header() {
     "
       >
         <Link to="/">
-          <div className="flex items-center gap-2">
-            <img src={header_logo} alt="header-logo" className="h-[40px] w-full pr-2" />
+          <div className="flex items-center">
+            <img src={header_logo} alt="header-logo" className="h-[40px] w-full pr-5" />
             <img
               src={header_name1}
               alt="header-name1"
-              className="h-[65px] min-w-[80px] pr-3 object-contain"
+              className="h-[65px] min-w-[90px] pr-5 object-contain"
             />
             <img
               src={header_name2}
               alt="header-name2"
-              className="h-[65px] min-w-[150px] pr-3 object-contain"
+              className="h-[65px] min-w-[160px] pr-5 object-contain"
             />
           </div>
         </Link>
