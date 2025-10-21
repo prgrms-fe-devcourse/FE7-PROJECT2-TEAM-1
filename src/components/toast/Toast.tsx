e4c2a92e7cab737d49bc7ce94014348c316193bd
leehyunjun97 <monsterstamp79@gmail.com>
2025-10-21 15:16:28 +0900

    🐛 fix: toast ui 프로젝트 컨셉과 비슷하게 매치

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -6,10 +6,14 @@ export default function Toast({ message, type }: { message: string; type: ToastT
   switch (type) {
     case "SUCCESS":
       return toast.success(message, {
-        className: "toast-custom",
+        className: "toast-success",
+        progressClassName: "toast-progress",
       });
     case "ERROR":
-      return toast.error(message);
+      return toast.error(message, {
+        className: "toast-error",
+        progressClassName: "toast-progress",
+      });
     case "INFO":
       return toast.info(message);
     default:
